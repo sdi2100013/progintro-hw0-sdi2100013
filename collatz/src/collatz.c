9026d7651d5945067e14569b5c6b07b2cadc2ddd
sdi2100013 <101860517+sdi2100013@users.noreply.github.com>
2023-11-08 13:45:45 +0200

    Update collatz.c

--- collatz/src/collatz.c
+++ collatz/src/collatz.c
@@ -1,44 +1,59 @@
 #include<stdio.h>
-
-#define MINNUM 100
-#define MAXNUM 1000000
+#include<stdlib.h>
 
 
-int main(){
-	int   seq , seqf , seqs = 0 ;
+
+int main(int argc, char **argv){
+	if (argc != 3) {
+		printf("You may better try tow numbers like : 12 289\n");
+ 		return 1;
+	}
+	int MINNUM = atoi(argv[1]);
+	int MAXNUM = atoi(argv[2]);
+	if(MAXNUM < MINNUM){
+		int swap;
+		swap = MAXNUM;
+		MAXNUM = MINNUM;
+		MINNUM = swap;
+	}
+	int   seq , seqf , seqs = 0, bignum, bseq ;
 	long long n, i, dig;
 	for(i = MINNUM; i <= MAXNUM; i++ ){
 		n = i;
-		if(n <= 0 ){
+		if(n <= 0 ){             
 			printf("0");
 			break;
 			return 0;
-		}
-		
+		}	
 		seqf = 2;
-		printf("\n%d  \n ", n);
 		while(n > 2){					
 			dig = n;
+			if(dig == bignum){       	    // in many sequences the numbers are reapeting them selfs 3:3 10 5 16 8 4 2 1
+				seqf = seqf + bseq;        //                                                       10: 10 5 16 8 4 2 1 
+				continue;                 // so we don't have to count the bigestnum with the bigest sequence(since the number we have on test)
+			}
 			if(dig % 2 == 0){
 				n/=2;
-				seqf++;
-				
+				seqf++;	
 			}
 			else{
 				n = 3 * n + 1;
 				n /= 2;						// AFTER N=3*N+1 ALWAYSS FOLLOWS N/=2
-				seqf = seqf + 2;
-				
+				seqf = seqf + 2;	
 			}
-	
 		}
 			 if (seqf > seqs){
 			 	seq = seqf;
 			 	seqs = seqf ;
+			 	bignum = n;
+			 	bseq = seq;
 			 }
 			 else{
 			 	seq = seqs ;
-			 };
+			 	bignum = n;
+			 	bseq = seq;
+			 }	 
 	}
 	printf(" \n bigest seq is %d ", seq);
 }
+
